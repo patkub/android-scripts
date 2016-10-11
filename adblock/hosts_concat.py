e1425a5489ec7824116cedc3707b9c097c102597
Patrick Kubiak <patrick@antuple.net>
2016-10-11 13:33:50 -0400

    attempt headers and log output

--- adblock/hosts_concat.py
+++ adblock/hosts_concat.py
@@ -1,3 +1,4 @@
+from sys import stdout
 import urllib
 from urllib.request import Request, urlopen
 
@@ -7,16 +8,19 @@ def main():
     hosts = []
 
     # read sources
+    stdout.write("Reading sources.txt\n")
     with open('sources.txt', 'r') as f:
         for source in f:
             if source and source.find('#') == -1:
                 get_source(source, hosts)
 
     # write hosts
+    stdout.write("Writing hosts...\n")
     with open('hosts', 'w') as f:
         for line in hosts:
             f.write(line + '\n')
 
+    print("Blocked " + format(len(hosts), ',d') + " hosts!")
 
 def get_source(source, hosts):
     '''
@@ -24,7 +28,7 @@ def get_source(source, hosts):
     :param source: url
     :param hosts: list to store hosts
     '''
-    data = parse_source(source).splitlines()
+    data = parse_source(source.rstrip()).splitlines()
     for line in data:
         if line and line.find('#') == -1 and line not in hosts:
             line = line.replace('\t', ' ')
@@ -37,11 +41,19 @@ def parse_source(url) -> str:
     :param url: url of source
     :return: string of source contents
     """
+    stdout.write("Parsing: " + url)
     try:
         req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         return urlopen(req).read().decode('utf-8')
     except urllib.error.HTTPError:
-        return urlopen(url).read().decode('utf-8')
+        stdout.write(" ...Header Failed!\n")
+        try:
+            stdout.write("\tAttempting without header")
+            return urlopen(url).read().decode('utf-8')
+        except urllib.error.HTTPError:
+            stdout.write("HTTP Failed!\n")
+    finally:
+        stdout.write(" ...OK!\n")
 
 
 if __name__ == '__main__':
