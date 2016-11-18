4e537e5caa501e20a4dd285dab4d4349cf9b1a4f
Patrick Kubiak <patrick@antuple.net>
2016-11-18 01:31:18 -0500

    use root over adb

--- README.md
+++
@@ -1,7 +0,0 @@
-AdBLock
-=======
-System hosts AdBlock file generator
-
-Freeform Windows
-================
-TWRP flashable zip to enable freeform windows.
--- adblock/flash_hosts.bat
+++ adblock/flash_hosts.bat
@@ -1,5 +1,6 @@
 echo Concatenating hosts...
 python hosts_concat.py sources.txt
 echo Flashing AdBlock...
+adb root
 adb pull /system/etc/hosts old_hosts
 adb push hosts /system/etc
